991794efa1cee800e515d7fd7acfd57c2e2c5ecb
ken munu <kentish.munusami@gmail.com>
2020-04-16 10:34:37 +0200

    ajout de cas de test selenium

--- src/test/java/com/example/selenium/SeleniumTest.java
+++ src/test/java/com/example/selenium/SeleniumTest.java
@@ -1,28 +1,36 @@
 package com.example.selenium;
 
-import java.util.Collections;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTest {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\kenti\\Documents\\dev\\chromedriver_win32\\chromedriver.exe");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        WebDriver driver =  new ChromeDriver(options); 
-        driver.get("https://accounts.google.com/signin");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='identifierId']"))).sendKeys("kentish.munusami@capfi.fr");
-        driver.findElement(By.id("identifierNext")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='password']"))).sendKeys("Marseille93240");
-        driver.findElement(By.id("passwordNext")).click();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
+//        options.setExperimentalOption("useAutomationExtension", false);
+//        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+//        WebDriver driver =  new ChromeDriver(options); 
+//        driver.get("https://accounts.google.com/signin");
+//        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='identifierId']"))).sendKeys("kentish.munusami@capfi.fr");
+//        driver.findElement(By.id("identifierNext")).click();
+//        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='password']"))).sendKeys("Marseille93240");
+//        driver.findElement(By.id("passwordNext")).click();
+		
+//		WebDriver driver=new ChromeDriver(); 
+//	    driver.get("http://localhost:4200/#/login");
+//	    driver.findElement(By.id("mat-input-0")).sendKeys("kentish.munusami@capfi.fr");
+//	    driver.findElement(By.id("mat-input-1")).sendKeys("admin");  
+//	    driver.findElement(By.name("connecter")).click();
+//	    driver.close();	
+		
+		                  
+		
+
+	    
 	}
 
 }
